2ed5cf3afddd38e09cad86e86a3d9f706985fd02
trindademarina <135060656+trindademarina@users.noreply.github.com>
2026-06-02 20:50:08 -0300

    Update teste1-14-04.ino

--- teste1-14-04.ino
+++ teste1-14-04.ino
@@ -5,13 +5,17 @@ Stepper motor(passosPorVolta, 4, 6, 5, 7);
 
 void setup() {
   motor.setSpeed(15);  // Velocidade em RPM (mais baixo = mais lento, mais preciso)
+  Serial.begin(9600);
 }
 
 void loop() {
-  for(int i = 0; i < 2; i++) {
-    motor.step(3072);      // da 1 volta
-    delay(2000); // Espera 2 segundos
-    motor.step(-2048);      // da 3 volta 
-    delay(2000); // Espera 2 segundos
-  }
+    if (Serial.available() > 0){
+      int comando = Serial.read();
+      if ( comando == 1 ){
+      motor.step(3072);      // da 1 volta
+      delay(2000); // Espera 2 segundos
+      motor.step(-2048);      // da 3 volta
+      delay(2000); // Espera 2 segundos
+      }
+    
 }
